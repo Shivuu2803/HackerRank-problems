f75a7f4fd30678d728babf198b510c5f4688b65d
Shivuu2803 <94588491+Shivuu2803@users.noreply.github.com>
2023-10-15 15:34:33 +0530

    Create 45-Largest Rectangle.cpp

---
+++ 45-Largest Rectangle.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+long largestRectangle(vector<int> h) 
+{
+    long iMax = 0;
+    
+    for(long iIdx = 0; iIdx < h.size(); ++iIdx) 
+    {
+        int iCount = 1;
+        long iLeft = iIdx -1;
+        while (iLeft >= 0 && h[iLeft] >= h[iIdx] ) 
+        {
+            iCount++;
+            iLeft--;
+        }
+        
+        long iRight = iIdx + 1;
+        while (iRight < h.size() && h[iRight] >= h[iIdx] ) 
+        {
+            iCount++;
+            iRight++;
+        }
+        
+        if ( iMax < iCount * h[iIdx] ) 
+        {
+            iMax = iCount * h[iIdx];
+        }
+    }
+    return iMax;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(n);
+
+    for (int i = 0; i < n; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    long result = largestRectangle(h);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
